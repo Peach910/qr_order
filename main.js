ef5ca46381fa702ed7296794d9f11f7c68d332c8
Takuto <takussk098ts@gmail.com>
2020-08-21 11:22:49 +0900

    個数選択機能追加

--- main.js
+++ main.js
@@ -1,7 +1,15 @@
-var cntA = 0;
-var cntB = 0;
-var cntC = 0;
+var sumA = 0;
+var sumB = 0;
+var sumC = 0;
 
 function addClick() {
-    
+    const numA = parseInt(document.getElementById("numA").value ,10);
+    const numB = parseInt(document.getElementById("numB").value ,10);
+    const numC = parseInt(document.getElementById("numC").value ,10);
+
+    sumA += numA;
+    sumB += numB;
+    sumC += numC;
+    //console.log(sumA);
+    //console.log(numA);
 }
