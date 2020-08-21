4fe9d644e4bb9c2dd7e5745debd02ffbb735af05
HalsSC <haruking.8686@gmail.com>
2020-08-21 14:55:29 +0900

    1455

--- admin.php
+++ admin.php
@@ -7,21 +7,20 @@
   </head>
   <body>
   <form method="get" action="admin.php">
+  <?php
     ID:<input type="text" name="shopname" /><br>
     pass:<input type="text" name="password" /><br>
     <input type="submit" value="送信" />
+    $shopname=$_REQUEST["shopname"];
+    $password=$_REQUEST["password"];
+    if ($shopname != "" && $password != ""){
+      if ($password == "0000"){
+        print "ログイン中:{$shopname}";
+      }else{
+        print "パスワードが間違っています";
+      }
+    }
+  ?>
   </form>
   </body>
 </html>
-
-<?php
-  $shopname=$_REQUEST["shopname"];
-  $password=$_REQUEST["password"];
-  if ($shopname != "" && $password != ""){
-    if ($password == "0000"){
-      print "ログイン中:{$shopname}";
-    }else{
-      print "パスワードが間違っています";
-    }
-  }
-?>
